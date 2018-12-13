4dd63d6249d77561b42891dc57569c312c9b73b7
hello <sfsrsarer@qq.com>
2018-12-13 22:00:34 +0800

    111

---
+++ regsave.php
@@ -0,0 +1,38 @@
+<?php
+	header("content-type:text/html;charset=utf-8");
+	//一、接收
+	$vipname=$_GET['username'];
+	$vippw=$_GET['userpass'];
+	//二、处理
+	//1、连接数据库服务器
+	$conn = mysql_connect("localhost","root","root");
+	if(!$conn){
+		die("连接失败".mysql_error());
+		//三、响应
+	}else{
+		//2、选择数据库
+		mysql_select_db("wuyin",$conn);
+		
+		//3、执行SQL语句
+		//3.1 查询
+		$sqlstr="select * from vip where vipname='$vipname'";
+		$result = mysql_query($sqlstr,$conn);//执行查询SQL语句，返回值是表格
+		$rows = mysql_num_rows($result);//返回$result表格有几行。
+		if($rows==0){
+			//3.2保存
+			$sqlstr="insert into vip (vipname,vippw) values ('$vipname','$vippw')";
+			$result = mysql_query($sqlstr,$conn);	
+			//4、关闭数据库
+			mysql_close($conn);
+			if($result==1){
+				//三、响应
+				echo "1";
+			}else{
+				echo "注册失败";
+			}
+		}else{
+			echo "手机号已被注册";
+		}
+	}
+	
+?>
